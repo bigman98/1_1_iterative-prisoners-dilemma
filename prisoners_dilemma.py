9ade3fcbaa657bef96869a81e97061c27756a2f0
LanceWilliamsHHS <lance.williams@students.sacs.k12.in.us>
2016-12-15 08:22:51 -0500

    Changed #7
    I changed a branched

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -2,8 +2,7 @@ from __future__ import print_function
 
 ##### 
 # Computer Science and Software Engineering
-# PLTW AP CS Principles
-# (c)2014 Project Lead The Way, Inc.
+# PLTW AP CS Principle
 #
 # Activity 1.3.9 Tools for Collaboration
 # Project 1.3.10 Collaborating on a Project
